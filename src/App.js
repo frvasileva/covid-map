2867fc74fed7bec57b9bdf63b1dd1fc1fdb73360
Fanka Bacheva <f.r.vasileva@gmail.com>
2020-11-17 17:22:41 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
 
 function App() {
+  axios.get(`https://corona.lmao.ninja/v2/countries`).then((res) => {
+    const persons = res.data;
+    console.log(persons);
+  });
+
+  function mapEffect({ leafletElement: map } = {}) {
+    let response;
+
+    try {
+      response = axios.get("https://corona.lmao.ninja/v2/countries");
+    } catch (e) {
+      console.log(`Failed to fetch countries: ${e.message}`, e);
+      return;
+    }
+
+    const { data = [] } = response;
+    console.log(data);
+  }
+
+  mapEffect();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="wrapper">
+            <MapContainer
+              center={[51.505, -0.09]}
+              zoom={13}
+              scrollWheelZoom={true}
+              className="map-wrapper"
+            >
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
